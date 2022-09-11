bae78dd104ee943ad6754cb1b655c50ad597b4ec
!Mensch <assassinste@gmail.com>
2022-09-11 11:04:10 +0800

    feat: Implementing page 'About'

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import About from './views/pages/About';
 import Home from './views/pages/Home';
 
 const Routes = () => {
   return (
     <Switch>
+      <Route exact path="/about" component={About} />
       <Route exact path="/" component={Home} />
     </Switch>
   );
