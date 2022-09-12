6b8a5fa2ac15e31481c7db78ad25425739f68bdb
!Mensch <assassinste@gmail.com>
2022-09-12 19:45:27 +0800

    dev: Getting the sales' data and display it

--- src/app/views/dashboard/dashboard-default-content.tsx
+++ src/app/views/dashboard/dashboard-default-content.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { getSalesAxios } from 'services/saleService';
+import { SaleType } from 'models/sale-type';
 
 const DashboardDefaultContent = () => {
+  const [sales, setSales] = useState<SaleType[]>([]);
+
+  useEffect(() => {
+    fetchSales();
+  }, []);
+
+  const fetchSales = async () => {
+    const { data } = await getSalesAxios();
+
+    setSales(data);
+    console.log(data);
+  };
+
   return (
     <div>
       <h1>DashboardDefaultContent</h1>
+      <h2>{sales.length}</h2>
     </div>
   );
 };
