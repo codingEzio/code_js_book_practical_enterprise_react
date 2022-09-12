fc757d03f1c4ea9c1a79e1bc6ffc0d6a3c774e2c
!Mensch <assassinste@gmail.com>
2022-09-12 08:18:20 +0800

    feat: Modifying the template with our navigation links

--- src/app/components/navigation-bar.tsx
+++ src/app/components/navigation-bar.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { colors } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -15,6 +14,10 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     menuButton: {
       marginRight: theme.spacing(2),
+    },
+    link: {
+      color: colors.deepOrange[50],
+      textDecoration: 'none',
     },
     title: {
       flexGrow: 1,
@@ -29,18 +32,33 @@ export default function NavigationBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Online Shop
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Link className={`${classes.link} ${classes.title}`} to={'/'}>
+            Online shop
+          </Link>
+
+          <Button color="inherit">
+            <Link to={'/'} className={`${classes.link}`}>
+              Home
+            </Link>
+          </Button>
+
+          <Button color="inherit">
+            <Link to={'/about'} className={`${classes.link}`}>
+              About
+            </Link>
+          </Button>
+
+          <Button color="inherit">
+            <Link to={'/dashboard'} className={`${classes.link}`}>
+              Dashboard
+            </Link>
+          </Button>
+
+          <Button color="inherit">
+            <Link to={'/login'} className={`${classes.link}`}>
+              Login
+            </Link>
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
