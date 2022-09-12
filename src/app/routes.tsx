42ccc95027ed80764e555d784d19823029d758a7
!Mensch <assassinste@gmail.com>
2022-09-12 08:24:10 +0800

    feat: Implementing 404 page and its logic

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import About from './views/pages/About';
 import Home from './views/pages/Home';
+import NotFound from './views/pages/NotFound';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/about" component={About} />
       <Route exact path="/" component={Home} />
+
+      <Route exact path="/not-found" component={NotFound} />
+      <Redirect exact from={'*'} to={'/not-found'} />
     </Switch>
   );
 };
