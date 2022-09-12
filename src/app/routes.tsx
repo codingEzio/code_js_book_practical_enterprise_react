457274e1ae365ae238117642c6efe67acc4b0012
!Mensch <assassinste@gmail.com>
2022-09-12 10:34:14 +0800

    feat: Implementing the barebone dashboard page (default content)

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -4,10 +4,28 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import About from './views/pages/About';
 import Home from './views/pages/Home';
 import NotFound from './views/pages/NotFound';
+import Dashboard from './layouts/dashboard-layout/dashboard';
+import DashboardDefaultContent from './views/dashboard/dashboard-default-content';
+import SettingsAndPravicy from './views/dashboard/settings-and-pravicy';
 
 const Routes = () => {
   return (
     <Switch>
+      <Route
+        path={'/dashboard'}
+        render={({ match: { path } }) => (
+          <Dashboard>
+            <Switch>
+              <Route
+                exact
+                path={path + '/'}
+                component={DashboardDefaultContent}
+              />
+            </Switch>
+          </Dashboard>
+        )}
+      ></Route>
+
       <Route exact path="/about" component={About} />
       <Route exact path="/" component={Home} />
 
