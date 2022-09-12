1e62fb823315b4d371714098598be7f6ee1306a6
!Mensch <assassinste@gmail.com>
2022-09-12 23:22:14 +0800

    feat: Reworked for responsive layout

--- src/app/layouts/dashboard-layout/dashboard.tsx
+++ src/app/layouts/dashboard-layout/dashboard.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 
 import DashboardSidebarNavigation from './dashboard-sidebar-navigation';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    height: '100%',
+    overflow: 'hidden',
+    width: '100%',
+  },
+  wrapper: {
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+    paddingTop: 64,
+    [theme.breakpoints.up('lg')]: {
+      paddingLeft: 256,
+    },
+  },
+  contentContainer: {
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+  },
+  content: {
+    flex: '1 1 auto',
+    height: '100%',
+    overflow: 'auto',
+  },
+}));
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Dashboard = ({ children }: Props) => {
+  const classes = useStyles();
+
   return (
     <Grid
       container
@@ -16,7 +47,12 @@ const Dashboard = ({ children }: Props) => {
       justify="flex-start"
       alignItems="flex-start"
     >
-      <DashboardSidebarNavigation /> {children}
+      <DashboardSidebarNavigation /> {''}
+      <div className={classes.wrapper}>
+        <div className={classes.contentContainer}>
+          <div className={classes.content}>{children}</div>
+        </div>
+      </div>
     </Grid>
   );
 };
