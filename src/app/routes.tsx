c116d4aa87fcce2dc803f2c0255c736485e00a4e
!Mensch <assassinste@gmail.com>
2022-09-14 15:22:29 +0800

    dev: Removing unnecessary imports

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,12 +3,9 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { LinearProgress } from '@material-ui/core';
 
-import About from './views/pages/About';
 import Home from './views/pages/Home';
 import NotFound from './views/pages/NotFound';
 import Dashboard from './layouts/dashboard-layout/dashboard';
-import DashboardDefaultContent from './views/dashboard/dashboard-default-content';
-import SettingsAndPravicy from './views/dashboard/settings-and-pravicy';
 
 const Routes = () => {
   return (
